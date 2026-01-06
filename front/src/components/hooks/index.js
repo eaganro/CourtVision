391cba5cdf020ac7ad799b6dfd249408862d6261
eaganro <rory.eagan@gmail.com>
2026-01-06 17:02:10 -0500

    Remove useAutoSelectGame hook and its export from index.js

--- front/src/components/hooks/index.js
+++ front/src/components/hooks/index.js
@@ -3,7 +3,6 @@ export { useQueryParams } from './useQueryParams';
 export { useLocalStorageState } from './useLocalStorageState';
 export { useWebSocket } from './useWebSocket';
 export { useGameData } from './useGameData';
-export { useAutoSelectGame } from './useAutoSelectGame';
 export { useGameTimeline } from './useGameTimeline';
 export { useElementWidth } from './useElementWidth';
 export { useCourtVision } from './useCourtVision';
