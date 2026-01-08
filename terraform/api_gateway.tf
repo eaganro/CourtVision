11307dc259f8961e095858465731f26e3e98ce74
eaganro <rory.eagan@gmail.com>
2026-01-07 21:30:14 -0500

    Add api gateway websocket production with throttle limits to terraform.

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -7,6 +7,18 @@ resource "aws_apigatewayv2_api" "websocket_api" {
   route_selection_expression = "$request.body.action"
 }
 
+# Keep WebSocket routes live without manual deployments.
+resource "aws_apigatewayv2_stage" "websocket_production" {
+  api_id      = aws_apigatewayv2_api.websocket_api.id
+  name        = "production"
+  auto_deploy = true
+
+  default_route_settings {
+    throttling_burst_limit = 200
+    throttling_rate_limit  = 100
+  }
+}
+
 # --- Integration: followDate ---
 resource "aws_apigatewayv2_integration" "ws_join_date" {
   api_id             = aws_apigatewayv2_api.websocket_api.id
