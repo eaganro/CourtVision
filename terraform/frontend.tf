9a675e278e9099ff90a4dc7f8e388cde5b84f6cb
Rory Eagan <rory.eagan@gmail.com>
2026-08-19 00:49:06 -0400

    Allow response headers policy deployment

--- terraform/frontend.tf
+++ terraform/frontend.tf
@@ -135,6 +135,10 @@ resource "aws_cloudfront_response_headers_policy" "frontend_security" {
   name    = "minutesmap-frontend-security"
   comment = "Browser security headers for frontend pages and assets"
 
+  # The deployment role manages this permission through Terraform. Apply the
+  # policy update before CloudFront attempts to create the headers policy.
+  depends_on = [aws_iam_policy.github_shard_compute]
+
   security_headers_config {
     content_security_policy {
       content_security_policy = join("; ", [
